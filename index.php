e435ea2c7dfa994ce062e0315cb5644b5ebfeab7
Funny Rose <fannyperretlombard@gmail.com>
2017-10-21 08:36:47 +0200

    First code of my life

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="cat.css">
+    <title>Wild Circus</title>
+</head>
+
+<title> Wild Circus </title>
+
+<!-- It's here that I can coding all the content of my first page of a website. Here three colors than I choosed : white, orange, black, with the logo's Cat (a remember of the Wild Code School's logo. -->
+
+    <body>
+
+        <header><!--titre de la page avec En-tête pour donner plus d'impact visuel au contenu-->
+            <div>
+        <h1>Wild Circus</h1>
+        <h2>Welcome to the first Circus with Wildcats !</h2>
+    </div>
+</header>
+
+<div class="contain">
+    <!--à partir d'ici je mets tout le contenu de la page après avoir mis les bases comme le titre 1 et le titre 2-->
+
+    <a:hover>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#shows">Shows</a></li>
+                <li><a href="#tickets">Tickets</a></li>
+                <li><a href="#aboutus">About Us</a></li>
+            </ul>
+        </nav>
+    </a:hover>
+
+    <section>
+        <h3 id="presentation">Home</h3>
+        <p>Once upon a time...<br/>
+            A very long time ago... Far far away...<br/>
+            In a strange and unknown country... Lived a population full of kindness and sweetness...<br/>
+            They cohabited with cute wild cats raised in the magic forest and these cats were more intelligent than the normal...<br/>
+            One day, the people of this strange country had an idea, doing an incredible show and build a wonderful circus !<br/>
+            That's why since many many years, the sweetest and wildest circus travels in the world.</p>
+    </section>
+    <section>
+        <p>So come to meet the softest and most talented animals of our circus who will more that only purr !</p>
+    </section>
+
+    <section>
+        <h3 id="shows">Shows</h3>
+        <h4>The Clown and The Persian's Cat</h4>
+        <div class=clearfix><img src="http://ekladata.com/miPbm2yCdDkFR3Z28hXXaT21TKk.jpg"alt="picture of a clown and a persian cat"/>This performance combine a magical friendship between Luke the Clown and Famous, his gorgeous cat.<br/> The legend tales us that Luke found him in Transilvania ten years ago.<br/> During this show you'll see the incredible and unique friendship of Human and an animal...
+        </div>
+    </section>
+
+    <section>
+        <h4>The Trumpet's Cat</h4>
+        <div class=clearfix><img src="http://www.rochester.edu/largepics/april-fools/thumbs/kitty8-hover.jpg"alt="picture of a fluffy cat with his trumpet"title="MEOWWWWW !"/>WhiteJack is a beautiful cat who can play trumpet since he was a kitten.<br/> In our group is called the future "Amstrong" !<br/> In his show you could listen some cat's music !</div>                </section>
+
+    <section>
+        <h4>The Acrobate's Cat</h4>
+        <div class=clearfix><img src="http://ic.pics.livejournal.com/artgr_211/70554312/56464/56464_900.jpg"alt="picture of a white acrobate's cat"/>He can jump, he can jump, he can fly and he can more...<br/>let's have a round of applause for the Acrobat's Cat !<br/>In this show you'll see a cute cat make a demonstration of the cat's skill !</div>
+    </section>
+
+    <section>
+        <h3 id="tickets">Tickets</h3>
+        <h4>World's Tour</h4>
+        <p>January 8th : France<br/>
+            February 14th : Spain<br/>
+            March 6th : England<br/>
+            April 1st : Monaco<br/>
+            May 6th : Croatia<br/>
+            June 21th : Portugal<br/>
+            July 24th : Maroc<br/>
+            August 15th : Italy<br/>
+            September 4th : Roumania<br/>
+            October 10th : Mexico<br/>
+            November 26th : China<br/>
+            Dicember 31st : Japan</p>
+    </section>
+
+    <section>
+        <p><input type="email" placeholder="Buy your ticket"><input type="submit"></p>
+    </section>
+
+    <section>
+        <p><em>Check the list of different dates and countries still available and buy your ticket !</em></p>
+    </section>
+
+    <section>
+        <h3 id="aboutus">About Us</h3>
+        <!--Ici on insère le JQuery avec une petite animation simple-->
+        <script src="https://code.jquery.com/jquery-1.10.2.js">WEBSITE</script>
+        <button class id="go">&raquo; WILD CIRCUS !</button>
+        <div class id="block">=====> Thank you for visit our website !</div>
+
+        <p>We are a small but strong <strong>International Company since 1960's</strong>. We are proud to say that our animals are treated with respect and kindness.</p>
+    </section>
+</div>
+
+<footer>
+    <ul>
+        <li>created by a <a href="mailto:fanny.perret@outlook.fr"title="Send me an email !">crazy Lady Cat !</a></li>
+        <li><a href="http://www.robozzle.com/user.aspx?name=funnyrose" class="robozzle-link-profile">Robozzle's Profile</a></li>
+        <li>My personal <a href="https://dash.generalassemb.ly/FunnyRose/build-your-own-personal-website">Website</a></li>
+    </ul>
+</footer>
+</body>
+
+</body>
+</html>
